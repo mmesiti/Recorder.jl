03e20424c75500b748962c8245afd284e110f1a7
Michele Mesiti <michele.mesiti@kit.edu>
2023-10-31 16:10:44 +0100

    User-provided recording struct accepted
    Possible step of lazy solution for #1
    
    ```
    mystate = Recorder.State()
    @record mystate 3:2:7 myfunc(myargs...)
    ```
    is now possible.
    If `mystate` is not provided, a global structure is used.
    
    Also, done some refactoring.

--- src/Recorder.jl
+++ src/Recorder.jl
@@ -1,157 +1,213 @@
 module Recorder
-using Base: remove_linenums!
+using Base: remove_linenums!, nothing_sentinel
 export @record,
     clear,
     create_regression_tests
 
 import Serialization
 
-return_values = Dict{String,Vector{Any}}()
-argumentss = Dict{String,Vector{Any}}()
-argumentss_post = Dict{String,Vector{Any}}()
-call_number = Dict{String,Int32}()
+struct State
+    return_values::Dict{String,Vector{Any}}
+    argumentss::Dict{String,Vector{Any}}
+    argumentss_post::Dict{String,Vector{Any}}
+    call_number::Dict{String,Int32}
+end
 
+State() = State(Dict(),Dict(),Dict(),Dict())
 
+gs = State()
 
-function _add_getkey!(expr, res)
-    args = expr.args[2:end]
+"""Adds the logic to  determine the key for recording."""
+function _add_getkey!(output::Expr, input::Expr)
+    args = input.args[2:end]
     # get module_implementing, and key
     argtypes = :(())
     for arg in args
         push!(argtypes.args, :(typeof($(esc(arg)))))
     end
-    push!(res.args, :(argtypes = $argtypes))
-    push!(res.args, :(method = which($(esc(expr.args[1])), argtypes)))
-    push!(res.args, :(module_implementing = method.module))
-    push!(res.args, :(name = method.name))
-    push!(res.args, :(key = replace(string(module_implementing) * "." * string(name),
+    push!(output.args, :(argtypes = $argtypes))
+    push!(output.args, :(method = which($(esc(input.args[1])), argtypes)))
+    push!(output.args, :(module_implementing = method.module))
+    push!(output.args, :(name = method.name))
+    push!(output.args, :(key = replace(string(module_implementing) * "." * string(name),
         r"^Main\." => s"")))
 end
 
-
-function _record_expr(expr)
-    args = expr.args[2:end]
+"""Creates the expression to record the function call."""
+function _record_expr(input::Expr,state)
+    args = input.args[2:end]
 
     # Start assembling macro
-    res = quote
+    output::Expr = quote
         args_input = []
         args_output = []
     end
-    _add_getkey!(expr, res)
+    _add_getkey!(output,input)
 
     # process each argument pre-call
     for arg in args
-        push!(res.args, :(push!(args_input, deepcopy($(esc(arg))))))
+        push!(output.args, :(push!(args_input, deepcopy($(esc(arg))))))
     end
 
     # record argument list in global dictionary
-    push!(res.args, :(record_arguments(key, args_input)))
+    push!(output.args, :(record_arguments(key, args_input, $(esc(state)))))
 
     # evaluate function
-    push!(res.args, :(output = $(esc(expr))))
-    push!(res.args, :(record_return_values(key, deepcopy(output))))
+    push!(output.args, :(output = $(esc(input))))
+    push!(output.args, :(record_return_values(key, deepcopy(output), $(esc(state)))))
     # increase call_number
-    push!(res.args, :(increase_call_no(key)))
+    push!(output.args, :(increase_call_no(key,$(esc(state)))))
 
     # process each argument post evaluation
     for arg in args
-        push!(res.args, :(push!(args_output, deepcopy($(esc(arg))))))
+        push!(output.args, :(push!(args_output, deepcopy($(esc(arg))))))
     end
 
     # record argument list in global dictionary (post evaluation)
-    push!(res.args, :(record_arguments_post(key, args_output)))
+    push!(output.args, :(record_arguments_post(key, args_output,$(esc(state)))))
 
     # leaving output at the end as the expression value
-    push!(res.args, :(output))
-    res
+    push!(output.args, :(output))
+    output
 end
 
-function _no_record_expr(expr)
-    res = quote end
-    _add_getkey!(expr, res)
-    push!(res.args, :(increase_call_no(key)))
-    push!(res.args, :($(esc(expr))))
-    res
+"""Return an expression where the function call is not recorded,
+   but the call counter is increased.
+"""
+function _no_record_expr(expr::Expr,state)
+    output::Expr = quote end
+    _add_getkey!(output,expr)
+    push!(output.args, :(increase_call_no(key,$(esc(state)))))
+    push!(output.args, :($(esc(expr))))
+    output
 end
 
-macro record(expr)
-    _record_expr(expr)
-end
+"""Creates an expression where function calls are recorded
+   only in the specified range.
+"""
+function _record_with_range(record_range,expr::Expr,state)
+    output::Expr = quote end
+    _add_getkey!(output,expr)
 
-macro record(record_range, expr)
-    res = quote end
-    _add_getkey!(expr, res)
+    if record_range !== nothing
+        condition = :(get_call_no(key,$(esc(state))) + 1 in $record_range)
+    else
+        condition = :(true)
+    end
 
-    condition = :(get_call_no(key) + 1 in $record_range)
 
     ifblock = :(
         if $condition
         else
         end
     )
-    ifblock.args[2] = _record_expr(expr)
-    ifblock.args[3] = _no_record_expr(expr)
-    push!(res.args, ifblock)
-    res
+    ifblock.args[2] = _record_expr(expr,state)
+    ifblock.args[3] = _no_record_expr(expr,state)
+    push!(output.args, ifblock)
+    output
 end
 
-return_values = Dict{String,Vector{Any}}()
-argumentss = Dict{String,Vector{Any}}()
-argumentss_post = Dict{String,Vector{Any}}()
-call_number = Dict{String,Int32}()
+# Macros
 
-function record_arguments(key, args_input)
-    if !haskey(argumentss, key)
-        argumentss[key] = []
+"""Record macro with a single expression.
+   Uses the global state in the Recorder module,
+   records every call to the function.
+"""
+macro record(expr::Expr)
+    _record_expr(expr,:(Recorder.gs))
+end
+
+"""Record macro with an additional range expression.
+   Uses the global state in the Recorder module.
+   Only records the calls in the specified range.
+   The range must be given in the form
+
+   <1st call to record>:<interval>:<last call to record>
+   and only every <interval>-th function call
+   will be recorded.
+"""
+macro record(range_expr::Expr, expr::Expr)
+    _record_with_range(range_expr,expr,:(Recorder.gs))
+end
+
+"""Record macro
+   using a provided Recorded.State
+   instead of the the global state in the Recorder module.
+"""
+macro record(state_expr::Symbol, expr::Expr)
+    _record_expr(expr,state_expr)
+end
+
+"""Record macro with an additional range expression,
+   using a provided Recorded.State
+   instead of the the global state in the Recorder module.
+
+   Only records the calls in the specified range.
+   The range must be given in the form
+
+   <1st call to record>:<interval>:<last call to record>
+   and only every <interval>-th function call
+   will be recorded.
+"""
+macro record(state_expr::Symbol,range_expr::Expr, expr::Expr)
+    _record_with_range(range_expr,expr,state_expr)
+end
+
+
+# Internal functions to be used output expressions
+
+function record_arguments(key, args_input,state::State)
+    if !haskey(state.argumentss, key)
+        state.argumentss[key] = []
     end
-    push!(argumentss[key], args_input)
+    push!(state.argumentss[key], args_input)
 end
 
-function record_return_values(key, return_value)
-    if !haskey(return_values, key)
-        return_values[key] = []
+function record_return_values(key, return_value,state::State)
+    if !haskey(state.return_values, key)
+        state.return_values[key] = []
     end
-    push!(return_values[key], return_value)
+    push!(state.return_values[key], return_value)
 end
 
-function record_arguments_post(key, args_output)
-    if !haskey(argumentss_post, key)
-        argumentss_post[key] = []
+function record_arguments_post(key, args_output,state::State)
+    if !haskey(state.argumentss_post, key)
+        state.argumentss_post[key] = []
     end
-    push!(argumentss_post[key], args_output)
+    push!(state.argumentss_post[key], args_output)
 end
 
-function clear()
+function clear(state::State=gs)
     function cleardict!(d)
         for k in keys(d)
             delete!(d, k)
         end
     end
-    cleardict!(return_values)
-    cleardict!(argumentss)
-    cleardict!(argumentss_post)
-    cleardict!(call_number)
+    cleardict!(state.return_values)
+    cleardict!(state.argumentss)
+    cleardict!(state.argumentss_post)
+    cleardict!(state.call_number)
     nothing
 end
 
-function get_call_no(key)
-    if !haskey(call_number, key)
-        call_number[key] = 0
+function get_call_no(key,state::State)
+    if !haskey(state.call_number, key)
+        state.call_number[key] = 0
     end
-    call_number[key]
+    state.call_number[key]
 end
 
-function increase_call_no(key)
-    if !haskey(call_number, key)
-        call_number[key] = 0
+function increase_call_no(key,state::State)
+    if !haskey(state.call_number, key)
+        state.call_number[key] = 0
     end
-    call_number[key] += 1
+    state.call_number[key] += 1
 end
 
-function create_regression_tests_data(key, namestem)
-    return_value = return_values[key]
-    argument = argumentss[key]
-    argument_post = argumentss_post[key]
+function create_regression_tests_data(key, namestem,state::State=gs)
+    return_value = state.return_values[key]
+    argument = state.argumentss[key]
+    argument_post = state.argumentss_post[key]
     output_filename = "regression_tests_$namestem.data"
 
     data = Dict("return_value" => return_value,
@@ -163,9 +219,9 @@ function create_regression_tests_data(key, namestem)
 
 end
 
-function create_regression_tests(key, namestem=key)
-    output_filename = create_regression_tests_data(key, namestem)
-    script_filename = "regression_tests_$namestem.jl"
+function create_regression_tests(key; namestem=key,state::State=gs)
+    output_filename = create_regression_tests_data(key, namestem,state)
+    script_filename = "regression_tests_$namestem.jl" # DEBUG
 
     fname = split(key, ".")[end]
     modhierarchstr = join(split(key, ".")[1:end-1], ".")
@@ -197,7 +253,7 @@ function create_regression_tests(key, namestem=key)
     sbuffer = IOBuffer()
     println(sbuffer, "using $modhierarchstr")
     for line in filecontentexpr.args
-        if typeof(line) != LineNumberNode
+        if line isa LineNumberNode
             println(sbuffer, line)
         end
     end
--- test/runtests.jl
+++ test/runtests.jl
@@ -14,39 +14,39 @@ using .TestModule
     end
 
 
-    @testset "Return Value is recorded and can be retrieved with Recorder.get_return_value" begin
+    @testset "Return Value is recorded and can be retrieved with Recorder.gs.return_values" begin
         @test begin
             clear()
             @record f3arg(4, 5, 6)
-            Recorder.return_values["TestModule.f3arg"] |> last == f3arg(4, 5, 6)
+            Recorder.gs.return_values["TestModule.f3arg"] |> last == f3arg(4, 5, 6)
         end
         @test begin
             clear()
             @record TestModule1.f(4, 5, 6)
-            Recorder.return_values["TestModule1.f"] |> last == TestModule1.f(4, 5, 6)
+            Recorder.gs.return_values["TestModule1.f"] |> last == TestModule1.f(4, 5, 6)
         end
     end
 
-    @testset "Arguments are recorded and can be retrieved with get_arguments" begin
+    @testset "Arguments are recorded and can be retrieved with Recorder.gs.argumentss" begin
         @test begin
             clear()
             @record f3arg(4, 5, 6)
-            Recorder.argumentss["TestModule.f3arg"] |> last == [4, 5, 6]
+            Recorder.gs.argumentss["TestModule.f3arg"] |> last == [4, 5, 6]
         end
         @test begin
             clear()
             @record TestModule1.f(4, 5, 6)
-            Recorder.argumentss["TestModule1.f"] |> last == [4, 5, 6]
+            Recorder.gs.argumentss["TestModule1.f"] |> last == [4, 5, 6]
         end
     end
 
-    @testset "Post-values of args are recorded and can be retrieved with get_arguments_post" begin
+    @testset "Post-values of args are recorded and can be retrieved with Recorder.gs.argumentss_post" begin
         @test begin
             v = [1, 2, 3]
             clear()
             @record push!(v, 4)
-            Recorder.argumentss["Base.push!"] |> last == [[1, 2, 3], 4] &&
-                Recorder.argumentss_post["Base.push!"] |> last == [[1, 2, 3, 4], 4]
+            Recorder.gs.argumentss["Base.push!"] |> last == [[1, 2, 3], 4] &&
+                Recorder.gs.argumentss_post["Base.push!"] |> last == [[1, 2, 3, 4], 4]
         end
     end
 
@@ -80,7 +80,7 @@ using .TestModule
             for i in 1:10
                 @record identity(i)
             end
-            Recorder.get_call_no("Base.identity") == 10
+            Recorder.gs.call_number["Base.identity"] == 10
         end
     end
 
@@ -91,13 +91,52 @@ using .TestModule
                 @record 3:2:7 identity(i)
             end
             try
-                Recorder.argumentss["Base.identity"] == [[3], [5], [7]]
+                Recorder.gs.argumentss["Base.identity"] == [[3], [5], [7]]
             catch
-                println(Recorder.return_values)
+                println(Recorder.gs.return_values)
                 false
             end
         end
     end
+
+    @testset "Use custom state instead of Recorder's global" begin
+        @test begin
+           clear()
+	       mystate = Recorder.State()
+           @record mystate identity(50)
+           Recorder.gs.argumentss == Dict() &&
+               mystate.argumentss["Base.identity"] == [[50]]
+        end
+    end
+
+    @testset "Use record range and custom state" begin
+        @test begin
+            clear()
+            mystate = Recorder.State()
+            for i in 1:10
+                @record mystate 3:2:7 identity(i)
+            end
+            try
+                Recorder.gs.argumentss == Dict() &&
+                   mystate.argumentss["Base.identity"] == [[3],[5],[7]]
+            catch
+                println(Recorder.gs.return_values)
+                println(mystate.return_values)
+                false
+            end
+        end
+    end
+
+    @testset "clear(mystate) works as intended" begin
+        mystate = Recorder.State()
+        for i in 1:10
+            @record mystate 3:2:7 identity(i)
+        end
+        @test length(mystate.argumentss["Base.identity"]) == 3
+        clear(mystate)
+        @test length(mystate.argumentss) == 0
+    end
+
 
     function setup_cleanup(test, namestem)
         clear()
@@ -120,7 +159,7 @@ using .TestModule
                 try
                     create_regression_tests("Base.identity")
                 catch
-                    println(Recorder.return_values)
+                    println(Recorder.gs.return_values)
                     false
                 else
                     "regression_tests_Base.identity.data" in readdir()
@@ -153,6 +192,22 @@ using .TestModule
 
     end
 
+    @testset "create_regression_test(fname,state) uses user-provided state" begin
+        @test begin
+            setup_cleanup("Base.identity") do
+                mystate = Recorder.State()
+                @record mystate identity(5)
+                @record mystate identity("hello")
+                @record mystate identity(["hello", 7])
+                create_regression_tests("Base.identity",state=mystate)
+                data = deserialize("regression_tests_Base.identity.data")
+                data["return_value"] == [5, "hello", ["hello", 7]] &&
+                    data["arguments"] == [[5], ["hello"], [["hello", 7]]] &&
+                    data["arguments_post"] == [[5], ["hello"], [["hello", 7]]]
+            end
+        end
+    end
+
     @testset "create_regression_tests(fname) creates script with tests" begin
         @test begin
             setup_cleanup("Base.identity") do
@@ -176,6 +231,17 @@ using .TestModule
                     occursin("@testset for i", filestring) &&
                     occursin("Base.identity", filestring) &&
                     occursin("@test ", filestring)
+            end
+        end
+    end
+
+    @testset "create_regression_test(fname,namestem) uses namestem in filenames" begin
+        @test begin
+            setup_cleanup("A_Namestem") do
+                @record identity(5)
+                @record identity("hello")
+                create_regression_tests("Base.identity",namestem="A_Namestem")
+                "regression_tests_A_Namestem.jl" in readdir()
             end
         end
     end
